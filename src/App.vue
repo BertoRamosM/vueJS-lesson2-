db7b0e7691d4d80be361d9eb10ba21326d1a38e5
ARamos <albertramos902@gmail.com>
2024-05-25 09:25:25 +0100

    query selector, in vue is template ref

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,27 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+ <h1>{{title}}</h1>
+ <input type="text" ref="name">
+ <button @click="handleClick">Click event</button>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      title: 'My first vue App'
+    }
+  },
+  methods: {
+    handleClick() {
+      console.log(this.$refs.name)
+    }
   }
 }
 </script>
 
+
+//query selector in js in vue template refs
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
@@ -22,5 +30,10 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+h1{
+  border-bottom: 1px solid #ddd;
+  display: inline-block;
+  padding-bottom: 10px;
 }
 </style>
