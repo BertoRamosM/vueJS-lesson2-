53d5cb279d640fffa8b210cb0eff27d0960f05a2
ARamos <albertramos902@gmail.com>
2024-05-26 16:25:48 +0100

    use of teleport to change the dom place of a component

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,57 @@
 <template>
- <h1>{{title}}</h1>
- <p>Welcome...</p>
- <div v-if="showModal">
- <Modal  theme="sale" @close="toggleModal">
-  <template v-slot:links>
-    <a href="">Sign up now</a>
-    <a href="">More info</a>
-  </template>
-  <h1>Ninja giveaway</h1>
-  <p>Grab your ninja swag for half price!</p>
+  <h1>{{ title }}</h1>
+  <p>Welcome...</p>
 
-</Modal >
- </div>
- <button @click="toggleModal">open modal</button>
 
- </template>
+  <teleport to=".modals" v-if="showModal">
+    <Modal theme="" @close="toggleModal">
+      <template v-slot:links>
+        <a href="">Sign up now</a>
+        <a href="">More info</a>
+      </template>
+      <h1>Ninja giveaway</h1>
+      <p>Grab your ninja swag for half price!</p>
+    </Modal>
+  </teleport>
+
+  <teleport to=".modals" v-if="showModal2">
+    <Modal @close="toggleModal2">
+      <h1>Sign up to the newslatter</h1>
+      <p>For updates and promo codes!</p>
+    </Modal>
+  </teleport>
+
+  <button @click="toggleModal">open modal</button>
+  <button @click="toggleModal2">open modal 2</button>
+</template>
 
 <script>
-import Modal from './components/Modal.vue'
+import Modal from "./components/Modal.vue";
 
 export default {
-  name: 'App',
+  name: "App",
 
   components: {
-    Modal
+    Modal,
   },
 
   data() {
     return {
-      title: 'My first vue App', 
-      header: "Sign up for the giveaway",
-      text: "Grab your ninja swag for half price",
-      showModal: false
-    }
+      title: "My first vue App",
+      showModal: false,
+      showModal2: false,
+    };
   },
   methods: {
     toggleModal() {
-      this.showModal = !this.showModal
-    }
-  }
-}
+      this.showModal = !this.showModal;
+    },
+    toggleModal2() {
+      this.showModal2 = !this.showModal2;
+    },
+  },
+};
 </script>
-
 
 //query selector in js in vue template refs
 <style>
@@ -53,12 +63,12 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
-h1{
+h1 {
   border-bottom: 1px solid #ddd;
   display: inline-block;
   padding-bottom: 10px;
 }
-.active{
+.active {
   background: blue;
 }
 </style>
--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -3,7 +3,7 @@
     <div class="modal" :class="{sale: theme === 'sale'}">
      <slot></slot>
      <div class="actions">
-      <slot name="links"></slot>
+      
      </div>
     </div>
   </div>
@@ -21,7 +21,7 @@ export default {
 }
 </script>
 
-<style scoped>
+<style>
 .modal{
   width: 400px;
   padding: 20px;
@@ -48,6 +48,21 @@ export default {
   font-style: normal;
 }
 
+.modal .actions{
+  text-align: center;
+  margin: 30px 0 10px 0;
+  color:#333
+}
+
+.modal .actions a{
+  color: #333;
+  padding: 8px;
+  border: 1px solid #eee;
+  border-radius: 4px;
+  text-decoration: none;
+  margin: 10px;
+}
+
 .modal.sale{
   background: crimson;
   color: white;
@@ -55,5 +70,9 @@ export default {
 
 .modal.sale h1{
   color: white;
+}
+
+.modal.sale .actions a{
+  color:white
 }
 </style>
